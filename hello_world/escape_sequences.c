d10000f9d03c444c900d7895079c3aec56596a66
JoaoHeit0r <joaoheitor.ac01@gmail.com>
2026-03-22 11:28:46 -0300

    escape sequences

---
+++ hello_world/escape_sequences.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	printf("Hello World!\n");
+	//\n quebra de linha, é chamado de escape sequence
+	//função printf() usada para printar texto
+	printf("I'm learning C");
+	return 0;
+}
+
+/*
+*escape sequence
+*começam por uma backslash \ 
+*e representam caracteres especiais 
+*que não podem ser escritos diretamente
+*/
+
+
+// Escape Sequence |	Description
+// \t				Inserts a horizontal tab	
+// \\				Inserts a backslash character (\)	
+// \"				Inserts a double quote character
